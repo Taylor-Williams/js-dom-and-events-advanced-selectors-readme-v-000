3827df12fa31720f9f1d90ee5c35a2dd49b11758
Taylor Williams <taylor@thewilliams.net>
2019-02-19 22:28:45 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -14,6 +14,6 @@ function ninjaBabySelector() {
 function divSelector() {
   return $('div.pics')
 }
-function paragraphSelector() {
-  return $('p')
+function firstListItem() {
+  return $('#pic-list li:first-child')
 }
